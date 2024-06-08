41afbafaed4b4e747af1e767f22b3ce0fae9f14b
OneSanchMan <39906100+OneSanchMan@users.noreply.github.com>
2024-06-09 00:47:41 +0500

    Time: 1009 ms (5.05%) | Memory: 35.8 MB (92.44%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,5 +1,7 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        if len(nums) < 2:
+            return False
         cur_rem = 0
         remainders = {0 : -1}
         for i in range(len(nums)):
