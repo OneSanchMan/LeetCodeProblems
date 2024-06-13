d369493282c093d76527c173a90daa6b286a8c60
OneSanchMan <39906100+OneSanchMan@users.noreply.github.com>
2024-06-14 03:00:25 +0500

    Time: 58 ms (67.14%) | Memory: 16.5 MB (38.97%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.py
@@ -1,3 +1,5 @@
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-        return sum([abs(sorted(seats)[i]-sorted(students)[i]) for i in range(len(seats))])
+        arr1 = sorted(seats)
+        arr2 = sorted(students)
+        return sum([abs(arr1[i]-arr2[i]) for i in range(len(seats))])
